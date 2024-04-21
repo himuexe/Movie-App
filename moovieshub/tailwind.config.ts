6a12cefb26609bf7cfdd7a5b65838386b62f959b
Himanshu Sharma <sharmaslov@gmail.com>
2024-04-21 12:42:43 +0530

    Update tailwind.config.ts

--- moovieshub/tailwind.config.ts
+++ moovieshub/tailwind.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -18,8 +18,8 @@ const config = {
       },
     },
     extend: {
-      flex:{
-        full: "0 0 100%"
+      flex: {
+        full: "0 0 100%",
       },
       keyframes: {
         "accordion-down": {
@@ -37,7 +37,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"),require("tailwind-scrollbar-hide")],
-} satisfies Config
+  plugins: [require("tailwindcss-animate"), require("tailwind-scrollbar-hide")],
+} satisfies Config;
 
-export default config
+export default config;
